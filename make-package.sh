aa6b582b3afe1c0dc6f86e13bdaaa7894f81a3a7
Scott Cline <scott@rs21.io>
2018-09-10 10:18:20 -0600

    Changes.

--- make-package.sh
+++ make-package.sh
@@ -11,5 +11,10 @@ if [-e $codecheckTarball ]; then
   rm -f $codecheckTarball
 fi
 
+echo "Directory listing:"
+ls -l
+
+sleep 120
+
 tar fcvz $releaseTarball index.html dist
 tar fcvz $codecheckTarball --exclude=build --exclude=dist --exclude=node_modules *
