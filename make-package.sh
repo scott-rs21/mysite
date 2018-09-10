be65331e88f47e6fc8aebd959e1bffead392167f
Scott Cline <scott@rs21.io>
2018-09-10 09:53:13 -0600

    Fixed tar command for release.tgz

--- make-package.sh
+++ make-package.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 tar fcvz release.tgz index.html dist
-tar fcvz code-check.tgz * --exclude=build --exclude=dist --exclude=node_modules
+tar fcvz code-check.tgz --exclude=build --exclude=dist --exclude=node_modules *
